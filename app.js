d8bd3b46413815e3fe917b99f3e065e62cda70fc
Windows User <test@test.de>
2015-04-13 11:25:49 +0200

    jetzt auch in bunt

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var http = require('http');
+var chalk = require("chalk");
 
 var server = http.createServer();
 
 server.on('request', function(req, res){
 
-  console.log('HTTP-Request gestartet');
+  console.log(chalk.green('HTTP-Request gestartet'));
   console.log('HTTP-Methode: '+req.method);
   console.log('Pfad: '+req.url);
 
@@ -18,6 +19,8 @@ server.on('request', function(req, res){
     console.log('HTTP-Request beendet');
 
     var message = JSON.parse(body);
+
+    console.log(chalk.green('Neue Nachricht von ' + message.user + ': ') + chalk.yellow(message.message));
 
     res.writeHead(200, "OK", {'Content-Type': 'text/plain'});
     res.write('Hallo '+message.user+', deine Nachricht wurde empfangen.');
